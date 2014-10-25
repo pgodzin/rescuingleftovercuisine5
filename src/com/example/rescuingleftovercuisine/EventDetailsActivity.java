dcda701a2f83d1163a0521b1dd7b3002820e73bf
Phil Godzin <pgg2105@columbia.edu>
2014-10-24 23:58:53 -0400

    Login logo changed

---
+++ src/com/example/rescuingleftovercuisine/EventDetailsActivity.java
@@ -0,0 +1,11 @@
+package com.example.rescuingleftovercuisine;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EventDetailsActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
