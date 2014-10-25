f8aa7c03a70cb1a65c00fe29e8f1d986873972be
Phil Godzin <pgg2105@columbia.edu>
2014-10-24 22:43:36 -0400

    Facebook login implemented

--- src/com/example/rescuingleftovercuisine/LoginActivity.java
+++ src/com/example/rescuingleftovercuisine/LoginActivity.java
@@ -1,15 +1,29 @@
 package com.example.rescuingleftovercuisine;
 
-import android.app.Activity;
-import android.os.Bundle;
 
-public class LoginActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Main Activity opened on Launch if not signed in
+ */
+public class LoginActivity extends FragmentActivity {
+    private LoginFragment mainFragment;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        if (savedInstanceState == null) {
+            // Add the fragment on initial activity setup
+            mainFragment = new LoginFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, mainFragment)
+                    .commit();
+        } else {
+            // Or set the fragment from restored state info
+            mainFragment = (LoginFragment) getSupportFragmentManager()
+                    .findFragmentById(android.R.id.content);
+        }
     }
 }
