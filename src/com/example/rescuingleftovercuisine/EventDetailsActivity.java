2889eb74e6a495bd39a3bf3bf44c3023d78949e3
Phil Godzin <pgg2105@columbia.edu>
2014-10-25 02:02:38 -0400

    event details activity

--- src/com/example/rescuingleftovercuisine/EventDetailsActivity.java
+++ src/com/example/rescuingleftovercuisine/EventDetailsActivity.java
@@ -1,12 +1,37 @@
 package com.example.rescuingleftovercuisine;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class EventDetailsActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_details);
+
+        TextView detailName = (TextView) findViewById(R.id.detail_name);
+        TextView detailTIme = (TextView) findViewById(R.id.detail_time);
+
+        detailName.setText(getIntent().getStringExtra("title"));
+        detailTIme.setText(getIntent().getStringExtra("time"));
+
+        Button start = (Button) findViewById(R.id.button_start);
+        Button end = (Button) findViewById(R.id.button_end);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //startActivityForResult;
+
+            }
+        });
+
     }
 }
--- src/com/example/rescuingleftovercuisine/EventsActivity.java
+++ src/com/example/rescuingleftovercuisine/EventsActivity.java
@@ -1,28 +1,15 @@
 package com.example.rescuingleftovercuisine;
 
 import android.app.Activity;
-import android.app.ListActivity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-
+import android.widget.*;
 import backend.Event;
 
-/**
- * Created by Damas on 10/24/14.
- */
+import java.util.ArrayList;
+
 public class EventsActivity extends Activity {
 
     ArrayAdapter<Event> adapter = null;
@@ -35,19 +22,16 @@ public class EventsActivity extends Activity {
 
         ListView eventsList = (ListView) findViewById(R.id.lv_events);
         //sample event Data
-        for(int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             Event test = new Event("Test Event " + i, "Today");
             eventData.add(test);
         }
 
-        adapter = new ArrayAdapter<Event>(this, android.R.layout.simple_list_item_2, android.R.id.text1, eventData){
+        adapter = new ArrayAdapter<Event>(this, android.R.layout.simple_list_item_2, android.R.id.text1, eventData) {
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
-//                LayoutInflater inflater = (LayoutInflater) getApplicationContext()
-//                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//
-//                View view = inflater.inflate(R.layout.activity_events, parent, false);
-                View view =  super.getView(position, convertView, parent);
+
+                View view = super.getView(position, convertView, parent);
 
                 TextView eventTitle = (TextView) view.findViewById(android.R.id.text1);
                 TextView eventTime = (TextView) view.findViewById(android.R.id.text2);
@@ -67,24 +51,17 @@ public class EventsActivity extends Activity {
                 Toast.makeText(getApplicationContext(), "Item " + position + " selected", Toast.LENGTH_SHORT).show();
 
                 //call detail activity to view details of this activity
-                /*
+
                 Event event = adapter.getItem(position);
-                Intent detail = new Intent(this, DetailActivity.class);
-                detail.putExtra(Intent.EXTRA_TEXT, event);
-                startActivity(detail); */
+                Intent detail = new Intent(EventsActivity.this, EventDetailsActivity.class);
+                String title = event.getEventTitle();
+                String time = event.getEventTime();
+                detail.putExtra("title", title);
+                detail.putExtra("time", time);
+
+                startActivity(detail);
             }
         });
     }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
 
 }
